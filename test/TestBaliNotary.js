2a167b0f5106bf306d6c27cb8509b36f07fe4539
Derk Norton <derk.norton@gmail.com>
2017-12-19 19:28:41 -0700

    Added a unit test for SHA512 hash.

--- test/TestBaliNotary.js
+++ test/TestBaliNotary.js
@@ -14,6 +14,20 @@ var testCase = require('nodeunit').testCase;
 
 
 module.exports = testCase({
+    'Test SHA512 Hash': function(test) {
+        test.expect(66);
+        for (var i = 0; i < 33; i++) {
+            var bytes = forge.random.getBytesSync(i);
+            var length = bytes.length;
+            var expected = i;
+            test.strictEqual(length, expected, 'The length of the random string is wrong: ' + length);
+            var hash = notary.sha512Hash(bytes);
+            length = hash.length;
+            expected = 64;
+            test.strictEqual(length, expected, 'The length of the hash string is wrong: ' + length);
+        }
+        test.done();
+    },
     'Test Signatures': function(test) {
         var keyPair = notary.generateKeyPair();
         var publicKey = keyPair.publicKey;
